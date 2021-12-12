fac5ac222183e7d8a07ff80a917057fe9b05818e
alexandru <boncut.alex10@gmail.com>
2021-12-11 23:32:26 -0600

    Add Feed component

---
+++ src/components/Feed.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tweet from "./Tweet";
+import Post from "./Post";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      <Tweet />
+
+      <Post />
+    </div>
+  );
+}
+
+export default Feed;
