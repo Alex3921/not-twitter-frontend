9b9aff7e3a2b72cc604f9ee3c51b4fdde28aa67b
alexandru <boncut.alex10@gmail.com>
2021-12-13 23:45:54 -0600

    Wrap avatar with a class name for styling

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -8,7 +8,9 @@ function Tweet() {
     <div className="tweet">
       <form>
         <div className="tweet__input">
-          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.newt/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <div className="tweet__avatar">
+            <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.newt/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          </div>
           <input placeholder="What's happening?" type="text" />
         </div>
         <Button className="tweet__submit-btn">Tweet</Button>
