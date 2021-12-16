a1844fd3b0d6cca94c3e5c6ca6e55c29860dce53
alexandru <boncut.alex10@gmail.com>
2021-12-15 22:28:54 -0600

    Wrap search input with a form tag

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./Widget.css";
 
-import SearchIcon from "@mui/icons-material/Search";
 import {
   TwitterTimelineEmbed,
-  TwitterShareButton,
   TwitterTweetEmbed,
 } from "react-twitter-embed";
 
@@ -12,8 +10,9 @@ function Widget() {
   return (
     <div className="widgets">
       <div className="widgets__search">
-        <SearchIcon className="widgets__searchIcon" />
-        <input placeholder="Search Twitter" type="text" />
+        <form className="nosubmit">
+          <input placeholder="Search Twitter" type="search" />
+        </form>
       </div>
 
       <div className="widgets__widgetContainer">
