15ba7d8e2e2bac5d23989ef01cceeb66e12b2f61
alexandru <boncut.alex10@gmail.com>
2021-12-17 22:59:48 -0600

    Wrap Provider around App component
    Create the store and pass it down to Provider

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import { createStore, applyMiddleware } from "redux";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
