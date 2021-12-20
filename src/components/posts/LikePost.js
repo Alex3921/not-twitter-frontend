77405546986adadb56b949fcace148bea22dc60c
alexandru <boncut.alex10@gmail.com>
2021-12-19 23:56:04 -0600

    Add Like component

---
+++ src/components/posts/LikePost.js
@@ -0,0 +1,9 @@
+import { FavoriteBorder } from "@mui/icons-material";
+
+export const LikePost = () => {
+  return (
+    <div className="element">
+      <FavoriteBorder fontSize="small" />
+    </div>
+  );
+};
