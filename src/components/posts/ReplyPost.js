051d57b35cbf78fb0441c636c741c2e409b83433
alexandru <boncut.alex10@gmail.com>
2021-12-19 23:56:31 -0600

    Add ReplyPost component

---
+++ src/components/posts/ReplyPost.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ChatBubbleOutline } from "@mui/icons-material";
+
+export const ReplyPost = () => {
+  return (
+    <div className="element">
+      <ChatBubbleOutline fontSize="small" />
+    </div>
+  );
+};
