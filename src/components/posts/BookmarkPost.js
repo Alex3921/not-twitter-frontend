622f6f397d11d37f903a40c8f799adc1043e4fab
alexandru <boncut.alex10@gmail.com>
2021-12-19 23:55:50 -0600

    Add Bookmark component

---
+++ src/components/posts/BookmarkPost.js
@@ -0,0 +1,9 @@
+import { Publish } from "@mui/icons-material";
+
+export const BookmarkPosts = () => {
+  return (
+    <div className="element">
+      <Publish fontSize="small" />
+    </div>
+  );
+};
