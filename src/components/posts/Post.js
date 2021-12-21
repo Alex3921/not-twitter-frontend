ed77caa8fa80d55e9ca5ea156109f0a0a441e02a
alexandru <boncut.alex10@gmail.com>
2021-12-20 23:57:32 -0600

    Import post_footer features

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReplyPost from "./ReplyPost";
-import SharePost from "./SharePost";
-import LikePost from "./LikePost";
-import BookmarkPost from "./BookmarkPost";
+import { ReplyPost } from "./ReplyPost";
+import { SharePost } from "./SharePost";
+import { LikePost } from "./LikePost";
+import { BookmarkPost } from "./BookmarkPost";
 import { Avatar, Button } from "@mui/material";
 import { Verified } from "@mui/icons-material";
 
-function Post(props) {
+function Post({post}) {
+  // debugger
   return (
     <>
+    {console.log(post)}
       <div className="post__avatar">
         <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.newt/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
       </div>
@@ -23,7 +25,7 @@ function Post(props) {
           </div>
         </div>
         <div className="post__content">
-          <p>I challenge you to build a Twitter Clone</p>
+          <p></p>
         </div>
         <div className="post__footer">
           <ReplyPost />
