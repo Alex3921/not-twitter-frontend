d4ec3042b15fad0766dccac8814d4df28849e48b
alexandru <boncut.alex10@gmail.com>
2021-12-20 23:55:59 -0600

    Update rootReducer path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "./reducers/rootReducer";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reportWebVitals from "./reportWebVitals";
