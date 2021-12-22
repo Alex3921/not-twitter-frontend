8ed14d374bfe79be1ac86a59c946eda1563f5569
Alex <boncut.alex10@gmail.com>
2021-12-21 23:55:58 -0600

    Add Page component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Navbar from "./components/navbar/Navbar";
-import Feed from "./components/Feed";
-import Widget from "./components/widget/Widget";
+import Pages from "./Pages"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <Feed />
-
-      <Widget />
+      <Pages />
     </div>
   );
 }
