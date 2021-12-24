636a46728eb22c9be452e92938b9a1e893cd1217
alexandru <boncut.alex10@gmail.com>
2021-12-23 23:54:34 -0600

    Move PostsContainer inside components dir

---
+++ src/components/containers/PostsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Posts from "../posts/Posts";
+
+import "./PostsContainer.css";
+
+export default class PostsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Posts />
+      </div>
+    );
+  }
+}
