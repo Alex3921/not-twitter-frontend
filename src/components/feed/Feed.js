e7dd1c0b28c9fef23f3194819768ad140f803702
alexandru <boncut.alex10@gmail.com>
2021-12-26 23:04:04 -0600

    Add routes to home/bookmarks/profile

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import HomePage from "../pages/HomePage"
+import HomePage from "../pages/HomePage";
+import Bookmarks from "../pages/BookmarksPage";
+import ProfilePage from "../pages/ProfilePage";
 import PostsContainer from "../containers/PostsContainer";
 import "./Feed.css";
 
 function Feed() {
   return (
     <div className="feed">
-     
-      <HomePage />
+      <Switch>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/bookmarks">
+          <Bookmarks />
+        </Route>
+        <Route path="/bookmarks">
+          <ProfilePage />
+        </Route>
+      </Switch>
       <PostsContainer />
     </div>
   );
