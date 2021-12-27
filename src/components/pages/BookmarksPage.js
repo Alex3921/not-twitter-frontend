c3261380de0e89ee14fdf109592c1a7fa863bf62
alexandru <boncut.alex10@gmail.com>
2021-12-26 23:05:41 -0600

    Add header for BookmarksPage

--- src/components/pages/BookmarksPage.js
+++ src/components/pages/BookmarksPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function BookmarksPage() {
   return (
     <div>
+      <div className="feed__header">
+        <h2>Bookmarks</h2>
+      </div>
       
+
     </div>
-  )
+  );
 }
 
-export default BookmarksPage
+export default BookmarksPage;
