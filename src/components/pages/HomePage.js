967f737bb94f82d993e832877606cfde6843112a
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:49:15 -0600

    Add needed imports and PostsContainer

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import Tweet from "../tweet/Tweet";
+import PostsContainer from "../containers/PostsContainer";
+
+import "../feed/Feed.css";
 
 export default class HomePage extends Component {
+
+  
   render() {
     return (
-      <div>
+      <div className="feed">
         <div className="feed__header">
           <h2>Home</h2>
         </div>
 
         <Tweet />
+        <PostsContainer />
+
       </div>
     );
   }
