e121a7b5055e6fb5043bceda4cc0b1273deb51c4
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:49:56 -0600

    Add routes for bookmarks and profile page

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -3,16 +3,46 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import Feed from "../feed/Feed";
 import Widget from "../widget/Widget";
+import SigninPage from "./SigninPage";
+
+import HomePage from "./HomePage";
+import BookmarksPage from "./BookmarksPage";
+import ProfilePage from "./ProfilePage";
+import PostsContainer from "../containers/PostsContainer";
+import "../feed/Feed.css";
 
 export default function Pages() {
   return (
     <Switch>
-      <Route path="/">
-        <Redirect to="/home" />
-        <Navbar />
-        <Feed />
-        <Widget />
-      </Route>
+      {console.log(localStorage)}
+      {localStorage.jwt ? (
+        <>
+          <Route exact path="/home">
+            <Navbar />
+            <HomePage />
+            <Widget />
+          </Route>
+
+          <Route exact path="/bookmarks">
+            <Navbar />
+            <BookmarksPage />
+            <Widget />
+          </Route>
+
+          <Route exact path="/profile/:username">
+            <Navbar />
+            <ProfilePage />
+            <Widget />
+          </Route>
+        </>
+      ) : (
+        <>
+          <Redirect to="/signin" />
+          <Route exact path="/signin">
+            <SigninPage />
+          </Route>
+        </>
+      )}
     </Switch>
   );
 }
