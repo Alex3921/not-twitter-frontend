c8e185ca49cc714e6cccf2f4cf667466464ee356
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:51:21 -0600

    Add SigninPage

---
+++ src/components/pages/SigninPage.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signinUser } from "../../actions/authActions";
+
+import "./SigninPage.css";
+
+export class SigninPage extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signinUser(this.state);
+    this.setState({
+      username: "",
+      password: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="signin__page">
+        <form onSubmit={this.handleSubmit}>
+          <div className="username">
+            <label htmlFor="username">Username</label>
+            <input 
+              type="text" 
+              name="username" 
+              onChange={this.handleChange} 
+              />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="submit__btn">
+            <input type="submit" placeholder="Submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signinUser: (user) => dispatch(signinUser(user)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SigninPage);
