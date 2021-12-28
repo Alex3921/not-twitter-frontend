41369a8b67fcd48505506403c738d163ec9647c7
alexandru <boncut.alex10@gmail.com>
2021-12-27 23:48:04 -0600

    Change url for profile page

--- src/components/navbar/Profile.js
+++ src/components/navbar/Profile.js
@@ -3,10 +3,11 @@ import { NavLink } from "react-router-dom"
 import "./NavbarOption.css";
 
 function Profile({ active, text, Icon }) {
+  const user = JSON.parse(localStorage.user);
   return (
     <div className="navbar__option">
       <Icon sx={{ fontSize: 30 }} />
-      <NavLink to={text.toLowerCase()} ><span>{text}</span></NavLink>
+      <NavLink to={`/profile/${user.username}`} ><span>{text}</span></NavLink>
     </div>
   );
 }
