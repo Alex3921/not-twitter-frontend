d6583595d489f16e3bb92a53976bf972d46249e6
alexandru <boncut.alex10@gmail.com>
2021-12-30 22:35:30 -0600

    Add fetchSavedPosts and savePost actions

--- src/actions/bookmarkActions.js
+++ src/actions/bookmarkActions.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import { BOOKMARKS } from "./apiEndpoints";
 
-function bookmarkActions() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export const fetchSavedPosts = () => {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_POSTS" });
+    fetch(`${BOOKMARKS}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.jwt}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch({ type: "FETCH_SAVED_POSTS", savedPosts: data.savedPosts })
+      );
+  };
+};
 
-export default bookmarkActions
+
+export const savePost = (postId) => {
+  return (dispatch) => {
+    fetch(`${BOOKMARKS}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.jwt}`,
+      },
+      body: JSON.stringify({post_id: postId}),
+    })
+      .then((response) => response.json())
+      .then((data) => alert(data.message));
+  };
+};
