328071a4883068bc0a26484dda12a967d6eb4ee5
alexandru <boncut.alex10@gmail.com>
2021-12-30 22:33:56 -0600

    Rename actionEndpoints => apiEndpoints

--- src/actions/actionEndpoints.js
+++
@@ -1,4 +0,0 @@
-export const SIGNUP = "http://localhost:3000/signup";
-export const SIGNIN = "http://localhost:3000/signin";
-export const LIKES = "http://localhost:3000/likes";
-export const POSTS = "http://localhost:3000/posts";
