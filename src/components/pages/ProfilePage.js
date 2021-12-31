99233710f02315b9af9b15d82047e713d9804970
alexandru <boncut.alex10@gmail.com>
2021-12-30 23:01:51 -0600

    Refactor into a class.
    -> add fetchTweets method and button that calls it
    -> add fetchLikes method and button that calls it

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,42 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import PostsContainer from "../containers/PostsContainer";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import FmdGoodIconOutlined from "@mui/icons-material/FmdGoodOutlined";
-import { useRouteMatch } from "react-router-dom";
 
 import "../feed/Feed.css";
 
-function ProfilePage() {
-  const user = JSON.parse(localStorage.user);
-  const joined = new Date(user.created_at);
-  const year = joined.getFullYear();
-  const month = joined.toLocaleString("default", { month: "long" });
+const user = localStorage.user ? JSON.parse(localStorage.user) : null;
+const joined = localStorage.user ? new Date(user.created_at) : null;
+const year = localStorage.user ? joined.getFullYear() : null;
+const month = localStorage.user ? joined.toLocaleString("default", { month: "long" }) : null;
 
-  let { url } = useRouteMatch();
+export default class ProfilePage extends Component {
+  state = { btnStatus: false };
 
-  return (
-    <div className="feed profile">
-      <div className="feed__header">
-        <h2>{user.name}</h2>
-        <h4>{"@" + user.username}</h4>
-        <div className="date__location">
-          <span className="location">
-            <FmdGoodIconOutlined fontSize="small" />
-            <span>{user.location}</span>
-          </span>
-          <span className="dateJoined">
-            <DateRangeIcon fontSize="small" />
-            <span>{"Joined " + month + " " + year}</span>
-          </span>
+  fetchTweets = () => {
+    this.setState({ btnStatus: true })
+    this.props.fetchUserPosts()
+  }
+
+  fetchLikes = () => {
+    this.setState({ btnStatus: false })
+    this.props.fetchLikedPosts()
+  }
+
+  componentDidMount() {
+    this.fetchTweets()
+  }
+
+
+  render() {
+    return (
+      <div className="feed profile">
+        <div className="feed__header">
+          <h2>{user.name}</h2>
+          <h4>{"@" + user.username}</h4>
+          <div className="date__location">
+            <span className="location">
+              <FmdGoodIconOutlined fontSize="small" />
+              <span>{user.location}</span>
+            </span>
+            <span className="dateJoined">
+              <DateRangeIcon fontSize="small" />
+              <span>{"Joined " + month + " " + year}</span>
+            </span>
+          </div>
         </div>
+        <div className="feed__header">
+          <button
+            className={this.state.btnStatus ? "active" : null}
+            onClick={() => this.fetchTweets()}
+          >
+            Tweets
+          </button>
+          <button
+            className={this.state.btnStatus ? null : "active"}
+            onClick={() => this.updateLikes()}
+          >
+            Likes
+          </button>
+        </div>
+        <PostsContainer />
       </div>
-      <div className="feed__header">
-        <button>Tweets</button>
-        <button>Likes</button>
-      </div>
-      <PostsContainer />
-    </div>
-  );
+    );
+  }
 }
-
-export default ProfilePage;
