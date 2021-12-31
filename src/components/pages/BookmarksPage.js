93ffd6852ca9c421fcd6aa90b4a7c6594f26260c
alexandru <boncut.alex10@gmail.com>
2021-12-30 22:52:02 -0600

    Add fetchSavedPosts on component mount

--- src/components/pages/BookmarksPage.js
+++ src/components/pages/BookmarksPage.js
@@ -1,27 +1,22 @@
 import React, { Component } from "react";
 import PostsContainer from "../containers/PostsContainer";
-import { connect } from "react-redux"
 
 import "../feed/Feed.css";
 
-export class BookmarksPage extends Component {
-  
+export default class BookmarksPage extends Component {
+  componentDidMount() {
+    this.props.fetchSavedPosts();
+  }
+
   render() {
-    console.log(this.props);
     return (
       <div className="feed">
         <div className="feed__header">
           <h2>Bookmarks</h2>
         </div>
-        
+
         <PostsContainer />
       </div>
-    )
-  };
+    );
+  }
 }
-
-const mapStateToProps = (state) => {
-  return state
-}
-
-export default connect(mapStateToProps)(BookmarksPage);
