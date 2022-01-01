5065b9e9ff14d37c2194440995905ae83459274a
alexandru <boncut.alex10@gmail.com>
2021-12-31 23:42:24 -0600

    Manage case for every page

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,22 @@
 const postsReducer = (state = [], action) => {
   switch (action.type) {
     case "LOADING_POSTS":
-      return [...state];
+      return [];
 
-    case "ADD_POSTS":
-      return [...state,...action.posts]
+    case "FETCH_HOME_POSTS":
+      return [...action.posts];
+
+    case "FETCH_SAVED_POSTS":
+      return [...action.savedPosts];
+
+    case "FETCH_LIKED_POSTS":
+      return [...action.likedPosts];
+
+    case "FETCH_USER_POSTS":
+      return [...action.userPosts];
+
+    case "ADD_POST":
+      return [action.post,...state];
 
     default:
       return state;
