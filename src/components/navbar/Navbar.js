ebf1dae69383b6c5e1598a375fac5b2541d23845
alexandru <boncut.alex10@gmail.com>
2021-12-31 23:53:07 -0600

    Add signOut method

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,24 @@
 import React from "react";
-// import NavbarOption from "./NavbarOption";
 import Home from "./Home";
-import Bookmarks from "./Bookmarks"
+import Bookmarks from "./Bookmarks";
 import Profile from "./Profile";
-import { NavLink } from "react-router-dom";
+
 import "./Navbar.css";
 
 import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
+// import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+// import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
 import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
 import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import Button from "@mui/material/Button";
 
 function Navbar() {
+  const signOut = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+  
   return (
     <div className="navbar">
       <TwitterIcon className="navbar__twitterIcon" />
@@ -28,6 +32,7 @@ function Navbar() {
         variant="outlined"
         className="navbar__signout-btn"
         fullWidth
+        onClick={() => signOut()}
       >
         Sign out
       </Button>
