8e8169d381e3c3f1c9fa618555c4a127ba7b5ca0
alexandru <boncut.alex10@gmail.com>
2022-01-01 23:30:09 -0600

    Use localStorage.username inside NavLink

--- src/components/navbar/Profile.js
+++ src/components/navbar/Profile.js
@@ -3,11 +3,10 @@ import { NavLink } from "react-router-dom"
 import "./NavbarOption.css";
 
 function Profile({ active, text, Icon }) {
-  const user = JSON.parse(localStorage.user);
   return (
     <div className="navbar__option">
       <Icon sx={{ fontSize: 30 }} />
-      <NavLink to={`/profile/${user.username}`} ><span>{text}</span></NavLink>
+      <NavLink to={`/profile/${localStorage.username}`} ><span>{text}</span></NavLink>
     </div>
   );
 }
