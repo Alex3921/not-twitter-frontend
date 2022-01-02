0bf13d4cdff37b55a57da97655540f2ef01b18bf
alexandru <boncut.alex10@gmail.com>
2022-01-01 23:22:21 -0600

    Import BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
 // import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
