10460f5880f69bf9d4efad5255733178d7356b76
alexandru <boncut.alex10@gmail.com>
2022-01-01 23:36:54 -0600

    Add likes reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import postsReducer from "./postsReducer";
 import usersReducer from "./usersReducer";
+import likesReducer from "./likesReducer";
 
 const rootReducer = combineReducers({  
   posts: postsReducer,
-  user: usersReducer
+  user: usersReducer,
+  likedPosts: likesReducer
 });
 
 export default rootReducer;
