52863d02396172ac4657982399ecbfbfda17a293
alexandru <boncut.alex10@gmail.com>
2022-01-06 23:48:00 -0600

    Add console.log for every action type

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,15 +1,22 @@
 const userReducer = (state = { isSignedIn: false, user: null }, action) => {
   switch (action.type) {
     case "LOADING":
+      console.log("Loading:" + state);
       return {...state};
 
     case "SIGNIN_SUCCESSFUL":
+      console.log("Success:" + state);
+
       return { isSignedIn: true, user: action.userData };
 
     case "SIGN_OUT":
+      console.log("Signout:" + state);
+
       return { isSignedIn: false, user: null };
 
     default:
+      console.log("Default:" + state);
+
       return state;
   }
 };
