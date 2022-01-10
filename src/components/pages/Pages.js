c1dfacebd682e682a4e4ceabe4447f67dd2829f2
alexandru <boncut.alex10@gmail.com>
2022-01-09 23:54:30 -0600

    Add console.log to profile route

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -38,6 +38,8 @@ export class Pages extends Component {
             </Route>
 
             <Route exact path="/profile/:username">
+            {console.log(this.props)}
+
               <ProfilePage
                 user={this.props.user.user}
                 fetchUserPosts={() => this.props.fetchUserPosts()}
