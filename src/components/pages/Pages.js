485d204eb17e0503a92247ee4751f1bd1ee15a78
alexandru <boncut.alex10@gmail.com>
2022-01-22 23:24:47 -0600

    Remove console.log

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -54,8 +54,6 @@ export class Pages extends Component {
         ) : (
           <>
             <Redirect to="/signin" />
-            {console.log(this.props)}
-
             <Route exact path="/signin">
               <SigninPage />
             </Route>
