ec5fa63ea1eb8272bb65d280336d9724c6a55038
Alexandru Boncut <boncut.alex10@gmail.com>
2022-02-09 23:49:19 -0600

    Add console.log

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -31,7 +31,7 @@ export class Pages extends Component {
             </Route>
 
             <Route exact path="/bookmarks">
-
+              console.log("Hey")
               <BookmarksPage
                 fetchSavedPosts={() => this.props.fetchSavedPosts()}
               />
