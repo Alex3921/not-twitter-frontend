b583ff3285f41777111607883e4716f0466c9a56
Alexandru Boncut <boncut.alex10@gmail.com>
2022-02-13 23:49:28 -0600

    .

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -37,7 +37,6 @@ export class Pages extends Component {
             </Route>
 
             <Route exact path="/profile/:username">
-
               <ProfilePage
                 user={this.props.user.user}
                 fetchUserPosts={() => this.props.fetchUserPosts()}
