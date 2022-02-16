5e13c4c6928c4213009eb0c03bc62443363023f7
Alexandru Boncut <boncut.alex10@gmail.com>
2022-02-15 23:04:36 -0600

    Remove console.log

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -67,7 +67,6 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  console.log("Dispatch")
   return {
     fetchSavedPosts: () => dispatch(fetchSavedPosts()),
     fetchLikedPosts: () => dispatch(fetchLikedPosts()),
