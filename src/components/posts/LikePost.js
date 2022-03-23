723aef9c812db9bd08c7e8b7970ff138b4f8e539
Alexandru Boncut <boncut.alex10@gmail.com>
2022-03-22 23:56:23 -0500

    Add console.log

--- src/components/posts/LikePost.js
+++ src/components/posts/LikePost.js
@@ -3,6 +3,7 @@ import { FavoriteBorder } from "@mui/icons-material";
 export const LikePost = (props) => {
   return (
     <div className="element">
+    {console.log("LikePost:", props)}
       <button onClick={() => props.likePost(props.postId)}>
         <FavoriteBorder className="heart" fontSize="small" />
       </button>
